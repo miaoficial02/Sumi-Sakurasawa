c858b94b575da48122b66675560acc6eb91d1e4a
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-07-15 00:59:46 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -75,10 +75,10 @@ global.correo = 'thekingdestroy507@gmail.com'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.catalogo = fs.readFileSync('./src/catalogo.jpg');
+global.catalogo = "https://files.catbox.moe/abc123.jpg";
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
-ch1: '120363416409380841@newsletter',
+ch1: '12000000000000000@newsletter',
 }
 global.multiplier = 60
 
