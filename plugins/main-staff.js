d1eb52afd87a0a07844e5d06d4e4b91ea950b6a7
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-07-17 15:29:35 -0300

    Update main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let img = './src/catalogo.jpg'
+let img = 'https://files.catbox.moe/92p8um.jpg'
 let staff = `ᥫ᭡ *EQUIPO DE AYUDANTES* ❀
 ✰ *Dueño* ${creador}
 ✦ *Bot:* ${botname}
