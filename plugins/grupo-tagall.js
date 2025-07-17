da8bd80b7caf492d9379c58c6aea0a9e3bd26d41
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-07-17 13:41:16 -0300

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -18,7 +18,7 @@ const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, co
   const oi = `*» INFO :* ${pesan}`;
   let teks = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄 ۪꒰ \`⡞᪲=͟͟͞${botname} ≼᳞ׄ\` ꒱ ۟ 𝅄 ┄\n`;
   for (const mem of participants) {
-    teks += `┊${customEmoji} @${mem.id.split('@')[0]}\n`;
+    teks += `𐔌ּࣱ໋᳝࣪${customEmoji} ᤢׅ࣫〬ᦡ̥ ᮫ׅ݅𑀝᮫ׅ̟۠݅ᡴּ໋࣪ @${mem.id.split('@')[0]}\n`;
   }
   teks += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *${vs}* ୧ ׅ ꒱  ┄  ─ ┄ ⸼`;
 
