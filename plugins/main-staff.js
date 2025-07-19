65f4244c56195f2c6825ec955bed18362836b559
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-07-19 00:50:16 -0300

    Update main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,1 +1,29 @@
+let handler = async (m, { conn, command, usedPrefix }) => {
+let img = './src/catalogo.jpg'
+let staff = `✿ *𝐄𝐐𝐔𝐈𝐏𝐎 𝐃𝐄 𝐀𝐘𝐔𝐃𝐀𝐍𝐓𝐄𝐒* ✿
+✰ *Dueño* ${creador}
+✦ *Bot:* ${botname}
+⚘ *Versión:* ${vs}
+❖ *Libreria:* ${libreria} ${baileys}
 
+❍ *Creador:*
+
+✿ 𝐈𝐓𝐒.𝐌𝐈𝐀.𝐎𝐅𝐈𝐂𝐈𝐀𝐋
+> 🜸 Rol » *Creador*
+> ✧ GitHub » https://github.com/miaoficial02
+
+❒ *𝐎𝐓𝐑𝐀𝐒 𝐑𝐄𝐃𝐄𝐒 𝐒𝐎𝐂𝐈𝐀𝐋𝐄𝐒:*
+
+ᰔ 𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌:↓
+> 𝐌𝐢𝐚 » *𝐂𝐑𝐄𝐀𝐃𝐎𝐑𝐀*
+> ✧ 𝐋𝐈𝐍𝐊 » https://instagram.com/its.mia.oficial
+`
+await conn.sendFile(m.chat, img, 'yuki.jpg', staff.trim(), fkontak)
+}
+  
+handler.help = ['staff']
+handler.command = ['colaboradores', 'staff']
+handler.register = true
+handler.tags = ['main']
+
+export default handler
